7959f634ae35b179a3d78c8d5ad60305fb3e478f
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-05-08 00:14:00 +0600

    displa page query is correct

--- CMS/admin/user.php
+++ CMS/admin/user.php
@@ -17,6 +17,7 @@
       <th scope="col">Full Name</th>
       <th scope="col">User Name</th>
       <th scope="col">Email</th>
+      <th scope="col">Role</th>
       <th scope="col">Edit</th>
       <th scope="col">Delete</th>
     </tr>
@@ -24,20 +25,14 @@
   <tbody>
   <?php  if($result) {
      while ($row = mysqli_fetch_assoc($result)) {
-      $sid = $_POST["userID"];
-      $firstName = $_POST["firstName"];
-      $lastName = $_POST["lastName"];
-      $email = $_POST["email"];
-      $userName = $_POST["userName"];
-      $pass = md5($_POST["pass"]);
-      $role =  $_POST["role"];
+
       echo'
       <tr>
-          <td>'.$sid.'</td>
-          <td>'.$firstName.' '.$lastName.'</td>
-          <td>'.$userName.'</td>
-          <td>'.$email.'</td>
-          <td>'.$role.'</td>
+          <td>'.$row["userId"].'</td>
+          <td>'.$row["firstName"]." ".$row["lastName"].'</td>
+          <td>'.$row["userName"].'</td>
+          <td>'.$row["email"].'</td>
+          <td>'.$row["role"].'</td>
           <td><button type="button" class="btn btn-warning">Edit</button></td>
           <td>
             <button type="button" class="btn btn-danger">Delete <a href="/crud/delete.php"></a> </button>
