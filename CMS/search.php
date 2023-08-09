2a8534781bfc38bb06e5bd44b42f920751c31179
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-08-09 08:13:41 +0600

    ad

--- CMS/search.php
+++ CMS/search.php
@@ -21,11 +21,12 @@ include_once "./admin/config.php";
         $limit = 3;
         if (isset($_POST['search'])) {
             $search = $_POST['searchInput'];
-            $searchQuery = "SELECT post_table.`Post_ID`, post_table.`Post_Title`, post_table.`Post_Description`, post_table.`Post_Date`, post_table.`Post_Image`,  post_table.`Category`, post_table.`Author`, categorytable.CategoryName, `usertable`.`userName` FROM post_table LEFT JOIN categorytable ON post_table.`category`= categorytable.Category_ID LEFT JOIN usertable ON `post_table`.`Author` = usertable.userId  ORDER BY post_table.Post_ID DESC LIMIT {$limit} WHERE `Post_ID`= '$search' or `Post_Title`= '$search' or `Post_Description` = '$search' ";
-          echo  $result = mysqli_query($conn, $searchQuery) or die("query faild");
+           echo $searchQuery = "SELECT post_table.`Post_ID`, post_table.`Post_Title`, post_table.`Post_Description`, post_table.`Post_Date`, post_table.`Post_Image`,  post_table.`Category`, post_table.`Author`, categorytable.CategoryName, `usertable`.`userName` FROM post_table LEFT JOIN categorytable ON post_table.`category`= categorytable.Category_ID LEFT JOIN usertable ON `post_table`.`Author` = usertable.userId   post_table.Post_ID  WHERE `Post_ID`= '$search' or `Post_Title`= '$search' or `Post_Description` = '$search' ORDER BY DESC LIMIT {$limit}";
+            $result = mysqli_query($conn, $searchQuery) or die("query faild");
             if (mysqli_num_rows($result)>0 ) {
                 // foreach ($result as $row) { 
-                    while($row = mysqli_fetch_assoc($result)){?>
+                    die();
+                    while($row = mysqli_fetch_assoc($result) > 0 ){?>
 
                     <div class="post_section">
                         <span class="bottom"></span>
